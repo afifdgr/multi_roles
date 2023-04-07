795d34d79002124f3fc5abef50b6265af5902f47
afifdgr <afifdgr3@gmail.com>
2023-04-07 09:31:22 +0700

    # Setup file routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
+import UserRoute from "./routes/UserRoute.js";
+import ProductRoute from "./routes/ProductRoute.js";
 dotenv.config();
 
 const app = express();
@@ -24,6 +26,8 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(UserRoute);
+app.use(ProductRoute);
 
 app.listen(process.env.APP_PORT, () => {
   console.log("Server up and running..");
